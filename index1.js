f78f9824fa9044f799e0202559d99bf9c985f255
I-Andrei <78320950+I-Andrei@users.noreply.github.com>
2022-06-25 15:59:28 +0300

    Reset button + indentation + another function

--- index1.js
+++ index1.js
@@ -1,22 +1,28 @@
- let jucator = 0;
+ let counter = 0;
  const cell = ['0', '1', '2', '3', '4', '5', '6', '7', '8']
 
- function x_Or_0(s){
+ function xOr0(s){
   let player = ""
-  if(jucator == 0 || jucator == 2 || jucator == 4 || jucator == 6 || jucator == 8){
+  if (counter == 0 || counter == 2 || counter == 4 || counter == 6 || counter == 8) {
     document.getElementById(s).innerHTML = "X"
     cell[s] = 'X'
     player = "X"
-  }
-  else{
+  } else {
     document.getElementById(s).innerHTML = "0"
     cell[s] = '0'
     player = "0"
   }
+  if (winnerOrDraw(counter) == 0)
+    document.getElementById('game').innerHTML = "Player " + player + " won!"
+  else if (winnerOrDraw(counter) == 1)
+    document.getElementById('game').innerHTML = "Draw!"
+  ++counter
+ }
+
+ function winnerOrDraw(counter) {
   if ((cell[0] == cell[1] && cell[1] == cell[2]) || (cell[3] == cell[4] && cell[4] == cell[5]) || (cell[6] == cell[7] && cell[7] == cell[8]) || (cell[0] == cell[3] && cell[3] == cell[6]) || (cell[1] == cell[4] && cell[4] == cell[7]) || (cell[2] == cell[5] && cell[5] == cell[8]) || (cell[0] == cell[4] && cell[4] == cell[8]) || (cell[6] == cell[4] && cell[4] == cell[2]))
-    document.getElementById('MyID').innerHTML = "Player " + player + " won!" 
-  if (jucator == 8)
-    document.getElementById('MyID').innerHTML = "Draw!"
-  ++jucator
+    return 0
+  if (counter == 8)
+    return 1
  }
  
